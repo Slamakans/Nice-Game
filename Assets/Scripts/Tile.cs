b6421be4f3ea1f4b7d0361b56e16c39b8ba34fa7
Blackchild <blackchidd@gmail.com>
2016-08-14 20:54:57 +0200

    Early level editor
    FEATURES
    Placing of tiles :+1:
    Multiple tilesheet support :+1:
    Things
    
    Changed quality settings to Fastest with full res textures.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Tile : MonoBehaviour {
+	public Texture2D Tilesheet;
+
+	public int TileSize;
+
+	private static Sprite[] TILE_SPRITES;
+	private int TEXTURE_ID;
+
+	private static Dictionary<string, Tile>[] tiles = {
+		new Dictionary<string, Tile>(),
+		new Dictionary<string, Tile>(),
+		new Dictionary<string, Tile>()
+	};
+
+	private void UpdateSprite()
+	{
+		if(TILE_SPRITES == null)
+			TILE_SPRITES = Resources.LoadAll<Sprite>("Sprites/Tilesheets/" + Tilesheet.name);
+
+		GetComponent<SpriteRenderer>().sprite = TILE_SPRITES[TEXTURE_ID];
+	}
+
+	public void SetId(int id)
+	{
+		if(TEXTURE_ID == id && GetComponent<SpriteRenderer>().sprite != null) return;
+		TEXTURE_ID = id;
+		UpdateSprite();
+	}
+
+	public void SetPos(Vector2 pos)
+	{
+		pos = ConvertToTilePos(pos);
+		if(GetTileAt(pos) != null)
+		{
+			var tile = GetTileAt(pos);
+			tile.SetId(TEXTURE_ID);
+			Destroy(gameObject);
+		}
+		else
+		{
+			transform.position = pos;
+		}
+	}
+
+	public static Vector2 ConvertToTilePos(Vector2 pos)
+	{
+		return new Vector2(Mathf.Floor(pos.x), Mathf.Floor(pos.y));
+	}
+
+	public static Tile GetTileAtLayer(Vector2 pos, int layer)
+	{
+		pos = ConvertToTilePos(pos);
+		Tile tile;
+		if(tiles[layer].TryGetValue(ConvertToKey(pos), out tile))
+		{
+			return tile.GetComponent<Tile>();
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public static Tile GetTileAt(Vector2 pos)
+	{
+		return GetTileAtLayer(pos, 0);
+	}
+
+	public static void CreateTileAtLayer(Transform prefab, Vector2 pos, int id, int layer)
+	{
+		var tile = Instantiate(prefab);
+		tile.GetComponent<Tile>().SetId(id);
+		tile.GetComponent<Tile>().SetPos(pos);
+		tiles[layer].Add(ConvertToKey(pos), tile.GetComponent<Tile>());
+	}
+
+	public static void CreateTile(Transform prefab, Vector2 pos, int id)
+	{
+		CreateTileAtLayer(prefab, pos, id, 0);
+	}
+
+	private static string ConvertToKey(Vector2 pos)
+	{
+		pos = ConvertToTilePos(pos);
+		return pos.x + "#" + pos.y;
+	}
+
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/Scripts/TilePlacingScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TilePlacingScript : MonoBehaviour {
+	public Transform TilePrefab;
+
+	private int currentId = 0;
+
+	void Update () {
+			Vector3 mousePos = Input.mousePosition;
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+		Debug.Log(Tile.GetTileAt(worldPos));
+		if(Input.GetButton("Fire1"))
+		{
+			if(Tile.GetTileAt(worldPos) == null)
+			{
+				Tile.CreateTile(TilePrefab, worldPos, currentId);
+			}
+			else
+			{
+				Tile.GetTileAt(worldPos).SetId(currentId);
+			}
+		}
+	}
+
+	void LateUpdate()
+	{
+		Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		var start = Tile.ConvertToTilePos(pos);
+		Debug.DrawRay(start, Vector2.right);
+		Debug.DrawRay(start, Vector2.up);
+		Debug.DrawRay(start + Vector2.right, Vector2.up);
+		Debug.DrawRay(start + Vector2.up, Vector2.right);
+	}
+}
